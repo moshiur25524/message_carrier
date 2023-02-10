d2bd1ea0894c2ff3a26e80294f63115afcd04351
moshiur25524 <moshiur.rahman25524@gmail.com>
2023-02-10 18:22:41 +0600

    blog section is made

--- js/app.js
+++ js/app.js
@@ -13,9 +13,12 @@ const displayNews = (allNews) => {
     const ul = document.getElementById('NewsLists')
     for (let news of allNews) {
         const li = document.createElement('li');
+        document.getElementById('category').innerText = `
+        ${allNews.length} Items found for category ${news.category_name}
+        `
         li.innerHTML = `
         <a onclick="NewsDetails('${news.category_id}')"
-        class="block cursor-pointer  py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+        class="block cursor-pointer py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
         aria-current="page">${news.category_name}</a>
         `
         ul.appendChild(li)
@@ -52,7 +55,7 @@ const displayNewsDetails = allDetails => {
         <div class="mb-8">
           
             <div class="text-black font-bold text-xl mb-2">${card.title} </div>
-            <p class="text-grey-darker text-base">${card.details.slice(0,300)}</p>
+            <p class="text-grey-darker text-base">${card.details.slice(0, 300)}</p>
         </div>
         <div class="flex justify-between container">
             <div class="flex items-center">
@@ -60,7 +63,7 @@ const displayNewsDetails = allDetails => {
                     src="${card.author.img}"
                     alt="Avatar of Jonathan Reinink">
                 <div class="text-sm">
-                     <p class="text-black leading-none">${card.author.name === ('null' || 'system') ? `<span class='text-red-700'>Missing Data</span>`: card.author.name }</p>
+                     <p class="text-black leading-none">${card.author.name === ('null' || 'system') ? `<span class='text-red-700'>Missing Data</span>` : card.author.name}</p>
                     <p class="text-grey-dark">Aug 18</p>
                 </div>
             </div>
